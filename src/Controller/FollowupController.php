5eee6254e3170e487f7bf34283ceeab74b3e73ff
CostaXX <dulcaconstantin24@gmail.com>
2024-11-14 11:59:43 +0100

    suivi sur veterinaire non fini

--- src/Controller/FollowupController.php
+++ src/Controller/FollowupController.php
@@ -7,10 +7,9 @@ use App\Form\FollowupType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\FollowUpRepository as RepositoryFollowUpRepository;
-use Symfony\Component\BrowserKit\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Repository\FollowUpRepository;
+use Symfony\Component\HttpFoundation\Response;
 
 #[Route('/followup')]
 class FollowupController extends AbstractController
@@ -41,5 +40,47 @@ class FollowupController extends AbstractController
             'followup' => $followup,
             'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_followup_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, FollowUp $followup, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(FollowupType::class, $followup);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_followup_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('followup/edit.html.twig', [
+            'followup' => $followup,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_followup_show', methods: ['GET'], requirements: ['id' => '\d+'])]
+    public function show(FollowUp $followup):Response
+    {
+        return $this->render('followup/show.html.twig', [
+            'followup' => $followup,
+        ]);
+    }
+
+    #[Route('/veterinary/{id}', name:'app_followup_showbyveterinaryid', methods: ['GET'], requirements: ['id' => '\d+'])]
+    public function showByVeterinaryId(FollowUp $followup): Response
+    {
+        
+    }
+
+    #[Route('/{id}', name: 'app_followup_delete', methods: ['POST'])]
+    public function delete(Request $request, FollowUp $followup, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$followup->getId(), $request->getPayload()->get('_token'))) {
+            $entityManager->remove($followup);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_followup_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/FollowupType.php
+++ src/Form/FollowupType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\FollowUp;
 use App\Entity\Veterinary;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,14 +16,16 @@ class FollowupType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('contactName')
-            ->add('comment')
+            ->add('contactName',TextType::class, ['label' => 'Contact'])
+            ->add('comment',TextType::class, ['label' => 'Commentaires'])
             ->add('callDate', null, [
                 'widget' => 'single_text',
+                'label' => 'Date d\'appel'
             ])
             ->add('veterinary', EntityType::class, [
                 'class' => Veterinary::class,
                 'choice_label' => 'id',
+                'label' => 'Véterinaire'
             ])
         ;
     }
